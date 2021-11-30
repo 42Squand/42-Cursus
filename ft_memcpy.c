4b83762507ef418b9ae913edb457e23a5c4617da
Mustafa Mujic <mmujic@c2r5s9.42lausanne.ch>
2021-11-30 14:08:47 +0100

    memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,20 +6,25 @@
 /*   By: mmujic <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 15:17:21 by mmujic            #+#    #+#             */
-/*   Updated: 2021/11/30 13:25:12 by mmujic           ###   ########.fr       */
+/*   Updated: 2021/11/30 14:07:02 by mmujic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void	*dest,	const void	*src, size_t	n);
+void	*ft_memcpy(void	*dest,	const void	*src, size_t	n)
 {
-	unsigned char	*original;
-	const unsigned char	*newcopy;
-	size_t	numb;
+	unsigned char		*original;
+	unsigned const char	*newcopy;
+	size_t				i;
 
 	original = dest;
 	newcopy = src;
-	numb = n;
-	while 
+	i = n;
+	while ((i < n) && (dest != NULL || src != NULL))
+	{
+		original[i] = newcopy[i];
+		i++;
+	}
+	return (dest);
 }
