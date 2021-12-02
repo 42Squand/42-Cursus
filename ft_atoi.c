451e2867fcceb2df94602dc60d23bcb078b7b253
42Squand <87322771+42Squand@users.noreply.github.com>
2021-12-02 11:35:02 +0100

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -10,4 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include  "libft.h"
 
+int ft_atoi(const char *nptr)
+{
+  int i;
+  int count;
+  int sign;
+  
+  i = 0;
+  count = 0;
+  sign = 1;
+  while (nptr[i] == ' ' || nptr[i] == '\t' || nptr[i] == '\n' ||
+        nptr[i] == '\f' || nptr[i] == '\r' || nptr[i] == '\v')
+  {
+        i++;
+  }
+  if (nptr[i] == '-' && nptr[i + 1] >= 48 && nptr[i + 1] <= 57)
+  {
+    i++;
+  }
+  while (nptr[i] >= 48 && nptr[i] <= 57)
+  {
+    count = count * 10 + nptr[i++] - '0';
+  }
+  if ((count != (count * 10) / 10) && (sign > 0))
+  {
+    return (-1);
+  }
+  if ((count != (count * 10) / 10) && (sign < 0))
+  {
+    return (0);
+  }
+  return (sign * count);
