b0ac75b457ddf8a9b3dd9d4e0fb47ad066a19c2d
42Squand <87322771+42Squand@users.noreply.github.com>
2021-12-02 12:19:14 +0100

    Update ft_putstr_fd.c

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -10,4 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 void ft_putstr_fd(char *s, int fd)
+{
+  if (s)
+  {
+    write(fd, s, ft_strlen(s));
+  }
+}
