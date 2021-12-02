034523e709b9230f26b710ec860e94fe63f1d55a
42Squand <87322771+42Squand@users.noreply.github.com>
2021-12-02 12:06:00 +0100

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -10,4 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void  *ft_memmove(void *dst, const void *src, size_t n)
+{
+  char        *dest;
+  const char  *sourc;
+  size_t      i;
+  
+  i = -1;
+  dest = (char *)dst;
+  sourc = (const char *)src;
+  if (sourc < dest)
+  {
+    i = n;
+    while (i > 0)
+    {
+      dest[i - 1] = sourc[i - 1];
+      i--;
+    }
+  }
+  else
+  {
+    while (i++ < n)
+    {
+      dest[i] = sourc[i];
+    }
+    return (dest);
+  }
