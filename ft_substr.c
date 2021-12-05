d44e945c3776925e23fc624892dd2ba0ad5df81e
Mustafa.M.42Lausanne <87322771+42Squand@users.noreply.github.com>
2021-12-05 23:36:18 +0100

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -11,4 +11,27 @@
 /* ************************************************************************** */
 
 char *ft_substr(char const *s, unsigned int start, size_t len)
-
+{
+  size_t  a;
+  size_t  b;
+  char    *str;
+  
+  str = (char*)malloc(sizeof(*s) * (len + 1));
+  if  (!str)
+  {
+    return (NULL);
+  }
+  a = 0;
+  b = 0;
+  while (s[a])
+  {
+    if (a >= start && j < len)
+    {
+      str[b] = s[a];
+      b++;
+    }
+    a++;
+  }
+  str[b] = 0;
+  return (str);
+}
