0178424cf7fc8be0bd6b7663c70d2efb22452e8f
Mustafa.M.42Lausanne <87322771+42Squand@users.noreply.github.com>
2021-12-07 11:56:27 +0100

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -10,4 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char *ft_strjoin(char const *s1, char const *s2)
+{
+  int   p1;
+  int   p2;
+  int   fusion;
+  char  *str;
+  
+  p1 = ft_strlen(s1);
+  p2 = ft_strlen(s2);
+  fusion = p1 + p2;
+  str = malloc((p1 + p2 + 1) * sizeof(char));
+  if (!str)
+  {
+    return (NULL);
+  }
+  if (!s1 || !s2)
+  {
+    return (NULL);
+  }
+  if (s1 && s2)
+  {
+    ft_memcpy(str, s1, p1);
+    ft_memcpy(str + p1, s2, p2);
+  }
+  str[fusion] = '\0';
+  return ((char *)str);
+}
