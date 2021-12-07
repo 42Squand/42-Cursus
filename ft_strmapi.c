b784df69581d50147d3de023ca2bcafd2528e620
Mustafa.M.42Lausanne <87322771+42Squand@users.noreply.github.com>
2021-12-07 12:02:33 +0100

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -10,4 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+  size_t  i;
+  char    *str;
+  
+  i = 0;
+  
