6f771f6076a4da6fade67615314bc83daf3cdd01
Mustafa.M.42Lausanne <87322771+42Squand@users.noreply.github.com>
2021-12-07 22:57:55 +0100

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -10,4 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strstrim (char const *s1, char const *set)
+#include "libft.h"
+
+char *ft_strtrim (const char *s1, const char *set)
+{
+  size_t  i;
+  size_t  len;
+  char    *str;
+  
+  if (!s1 || !set)
+  {
+    return (NULL);
+  }
+  i = 0;
+  while (s1[i] != '\0' && ft_strchr(set, s1[i]))
+  {
+    i++;
+  }
+  len = ft_strlen(s1 + i);
+  while (ft_strchr(set, s1[i + len]))
+  {
+    len--;
+  }
+  if (!(str = ft_substr(s1, i, len + 1)))
+  {
+    return (NULL);
+  }
+  return (str);
+}
