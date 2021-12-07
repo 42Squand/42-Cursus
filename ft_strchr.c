cf5cd035660634a867ef1957764a2c63103c265a
Mustafa.M.42Lausanne <87322771+42Squand@users.noreply.github.com>
2021-12-07 11:40:28 +0100

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -10,4 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include  "libft.h"
 
+char  *ft_strchr(const char *str, int c)
+{
+  size_t i;
+  
+  i = 0;
+  while (i < ft_strlen(str) + 1)
+  {
+    if (str[i] == (char)c)
+    {
+      return ((char *)str + i);
+    }
+    i++;
+  }
+  return (NULL);
+}
