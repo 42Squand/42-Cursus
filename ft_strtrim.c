cff4062481a3e15f2f53329672c97176c3bd432e
Mustafa.M.42Lausanne <87322771+42Squand@users.noreply.github.com>
2021-12-09 02:17:43 +0100

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,6 +12,54 @@
 
 #include "libft.h"
 
-char *ft_strtrim (const char *s1, const char *set)
+int ft_check(char c, char const *set)
 {
+  int i;
   
+  i = 0;
+  while (set[i])
+  {
+    if (set[i++] == c)
+    {
+      return (1):
+    }
+    return (0);
+  }
+}
+
+char  *ft_strtrim(char const char *s1, char const *set)
+{
+  char  *str;
+  int   i;
+  int   start;
+  int   end;
+  
+  i = 0;
+  start = 0;
+  if (s1 == 0 || set == 0)
+  {
+    return (0);
+  }
+  end = (int)ft_strlen(s1);
+  while (s1[start] && ft_check(s1[start], set))
+  {
+    start++;
+  }
+  while (end > start && ft_check(s1[end - 1], set))
+  {
+    end--;
+  }
+  str = (char *)malloc(sizeof(char) * (end - start + 1));
+  if (!str)
+  {
+    return (0);
+  }
+  while (start < end)
+  {
+    str[i] = s1[start];
+    i++;
+    start++;
+  }
+  str[i] = '\0';
+  rerturn (str);
+}
