6aa6bfe8aa18f7121a7fdb83464cbfc26b516031
Mustafa.M.42Lausanne <87322771+42Squand@users.noreply.github.com>
2021-12-13 16:07:59 +0100

    Update ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -12,25 +12,27 @@
 
 #include "libft.h"
 
-void ft_putnbr_fd (int n, int fd)
+
+void	ft_putnbr_fd(int n, int fd)
 {
-  if (n == -2147483648)
-  {
-    ft_putchar_fd('-', fd);
-    ft_putchar_fd('2', fd);
-    ft_putnbr_fd(147483648, fd);
-  }
-  else
-  {
-    if (n < 0)
-    {
-      n *= -1;
-      ft_putchar_fd('-', fd);
-    }
-    if (n >= 10)
-    {
-      ft_putnbr_fd(n / 10, fd);
-      ft_putchar_fd((n % 10) + 48, fd);
-    }
-  }
-}
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n *= -1;
+		ft_putnbr_fd(n, fd);
+	}
+	else if (n <= 9)
+	{
+		n += '0';
+		ft_putchar_fd(n, fd);
+	}
+	else
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
