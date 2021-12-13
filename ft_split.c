2b3b1fffeef088a98d028f5ff672ae722161974d
Mustafa.M.42Lausanne <87322771+42Squand@users.noreply.github.com>
2021-12-13 14:59:16 +0100

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -8,66 +8,69 @@
 /*   Created: 2021/10/22 13:06:26 by mmujic            #+#    #+#             */
 /*   Updated: 2021/10/22 13:06:59 by mmujic           ###   ########.fr       */
 /*                                                                            */
-/* *************************************************************************
+/* ************************************************************************
 
-static void	*ft_free_all(char **bigtab, int i)
-{
-	while (i--)
-		free(bigtab[i]);
-	free(bigtab);
-	return (NULL);
-}
 
-static int	ft_words_count(char const *str, char c)
+static int	ft_count_words(const char *s, char c)
 {
+	int	count;
 	int	i;
-	int	words;
 
+	count = 0;
 	i = 0;
-	words = 0;
-	if (str[i] == c || str[i] == '\0')
-		words--;
-	while (str[i])
+	if (s[i] == c || s[0] == '\0')
+		count--;
+	while (s[i])
 	{
-		if (str[i] == c && str[i + 1] != c && str[i + 1] != '\0')
-			words++;
+		if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
+			count++;
 		i++;
 	}
-	return (words + 1);
+	return (count + 1);
 }
 
-static int	ft_word_len(const char *str, char c)
+static int	ft_strlen_c(const char	*s, char c)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] && str[i] != c)
+	while (s[i] && s[i] != c)
 		i++;
 	return (i);
 }
 
-char	**ft_split(char const *str, char c)
+static void	*ft_free_all(char **res, int i)
 {
-	int		i;
-	int		wlen;
-	char	**bigtab;
+	while (i--)
+		free(res[i]);
+	free(res);
+	return (NULL);
+}
 
+char	**ft_split(char const *s, char c)
+{
+	char	**res;
+	int		i;
+	int		countwords;
+
+	if (!s)
+		return (NULL);
 	i = 0;
-	if (!str)
+	countwords = ft_count_words(s, c);
+	res = malloc(sizeof(char *) * (countwords + 1));
+	if (!res)
 		return (NULL);
-	wlen = ft_words_count(str, c);
-	bigtab = malloc((wlen + 1) * sizeof(char *));
-	if (!bigtab)
-		return (NULL);
-	while (i < wlen)
+	i = 0;
+	while (i < countwords)
 	{
-		while (*str == c && *str)
-			str++;
-		bigtab[i] = ft_substr(str, 0, ft_word_len(str, c));
-		if (bigtab[i] == NULL)
-			return (ft_free_all(bigtab, i));
-		str += ft_word_len(str, c);
+		while (*s == c && *s)
+			s++;
+		res[i] = ft_substr(s, 0, ft_strlen_c(s, c));
+		if (res[i] == NULL)
+			return (ft_free_all(res, i));
+		s += ft_strlen_c(s, c);
 		i++;
 	}
-	bigtab[i] = NULL;
-	return (bigtab);
+	res[i] = NULL;
+	return (res);
+}
