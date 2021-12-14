2351a6fb28e82c3292242ee6985b7c59b3138264
Mustafa Mujic <mmujic@c1r14s2.42lausanne.ch>
2021-12-14 16:27:17 +0100

    test02

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: mmujic <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 15:01:05 by mmujic            #+#    #+#             */
-/*   Updated: 2021/12/13 13:00:50 by mmujic           ###   ########.fr       */
+/*   Updated: 2021/12/14 16:12:50 by mmujic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,11 @@
 
 int	ft_strlen(const char *str)
 {
-	char	charnum;
+	int	charnum;
 
 	charnum = 0;
-	while (*str != '\0')
+	while (str[charnum] != '\0')
 	{
-		str++;
 		charnum++;
 	}
 	return (charnum);
