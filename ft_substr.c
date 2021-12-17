5889a9d2c698c2b840b22b9bbf4144898a5c8097
Mustafa.M.42Lausanne <87322771+42Squand@users.noreply.github.com>
2021-12-17 13:00:11 +0100

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -13,20 +13,3 @@
 #include "libft.h"
 
 char	*ft_substr(const char *s, unsigned int start, size_t len)
-{
-	size_t	i;
-	char	*str;
-
-	i = -1;
-	if (!s)
-		return (NULL);
-	if (len > ft_strlen(s))
-		len = ft_strlen(s);
-	str = malloc((len + 1) * sizeof(char));
-	if (!str)
-		return (NULL);
-	while (++i < len && start < ft_strlen(s))
-		str[i] = s[start++];
-	str[i] = '\0';
-	return (str);
-}
